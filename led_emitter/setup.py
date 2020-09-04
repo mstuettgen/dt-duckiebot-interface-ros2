f2191106a29f3653ee9b75f6c12e971c7b85a43d
Marcel Stüttgen <stuettgen@fh-aachen.de>
2020-09-04 22:25:41 +0200

    hopefully fixed setup.py

--- led_emitter/setup.py
+++ led_emitter/setup.py
@@ -8,14 +8,15 @@ package_name = 'led_emitter'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=['rgb_led'],
-    package_dir={'': 'include'},
+    packages=[package_name],
+#    package_dir={'': 'include'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+#        (os.path.join('share', package_name, 'include'), glob('include/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
